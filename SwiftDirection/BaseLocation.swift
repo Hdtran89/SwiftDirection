8470407bef768c78ffeebb9a6f392bfd2f5d1a2f
Hdtran89 <hdtran89@yahoo.com>
2014-11-22 18:51:51 -0600

    add get current location

--- SwiftDirection/BaseLocation.swift
+++ SwiftDirection/BaseLocation.swift
@@ -8,9 +8,36 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
-class BaseLocation: UIViewController
+class BaseLocation: UIViewController,CLLocationManagerDelegate
 {
+    
+    let locationManager = CLLocationManager()
+    var xcurrentcord: Float = 0
+    var ycurrentcord: Float = 0
+    var location:CLLocation?
+    
+    @IBAction func getLocation()
+    {
+        let authStatus:CLAuthorizationStatus = CLLocationManager.authorizationStatus()
+        
+        if (authStatus == .NotDetermined)
+        {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if authStatus == .Denied || authStatus == .Restricted {
+            showLocationServicesDeniedAlert()
+            return
+        }
+        
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+    }
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -20,7 +47,54 @@ class BaseLocation: UIViewController
         super.didReceiveMemoryWarning()
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    
+    override func prepareForSegue( segue: UIStoryboardSegue,
+                                  sender: AnyObject?)
+    {
         
+    }
+    
+    func saveCoordinate()
+    {
+        if let location = location{
+            var xstring:NSString = ""
+            var ystring:NSString = ""
+            
+            xstring = NSString(format: "%.8f", location.coordinate.longitude)
+            ystring = NSString(format: "%.8f", location.coordinate.latitude)
+            
+            println("%y: \(ystring)")
+            println("%x: \(xstring)")
+            
+            xcurrentcord = (xstring as NSString).floatValue
+            ycurrentcord = (ystring as NSString).floatValue
+            
+            println("floaty:  \(ycurrentcord)")
+            println("floatx:  \(xcurrentcord)")
+        }
+    }
+    func showLocationServicesDeniedAlert()
+    {
+        let alert = UIAlertController(title: "Location Service Disabled",
+                                    message: "Please enable location service in the app Settings",
+                             preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK",
+                                     style: .Default,
+                                   handler: nil)
+        alert.addAction(okAction)
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    //Mark -- Delegation CLLocationManager
+    func locationManager(  manager:CLLocationManager!,
+            didFailWithError error: NSError!)
+    {
+        println("didFailWithError \(error)")
+    }
+    
+    func locationManager(    manager: CLLocationManager!,
+        didUpdateLocations locations: [AnyObject]!)
+    {
+        let newLocation = locations.last as CLLocation
+        println("didUpdateLocation \(newLocation)")
     }
 }
